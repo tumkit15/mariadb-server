744ae89711b8c92eabccc974ade42bf36ef25805
Daniel Black <grooverdan@users.sourceforge.net>
2017-04-02 22:25:04 +1000

    travis: MYSQL_TEST_SUITES - tests for clang components
    Since using container builds is faster, and clang compiles
    are quicker due to tokudb and mgroonga being omitted, they
    have time in the 50 minute test window to run through the
    other test suites.

--- .travis.compiler.sh
+++ .travis.compiler.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 if [[ "${TRAVIS_OS_NAME}" == 'linux' && "${CXX}" == 'clang++' ]]; then
   case ${GCC_VERSION} in
-    4.8) MYSQL_BUILD_CXX=clang++-3.8;;
-    5) MYSQL_BUILD_CXX=clang++-3.9;;
-    6) MYSQL_BUILD_CXX=clang++-4.0;;
+    4.8) MYSQL_BUILD_CXX=clang++-3.8; MYSQL_TEST_SUITES=main,optimizer_unfixed_bugs,parts,sys_vars,unit,vcol,innodb,innodb_gis,innodb_zip,innodb_fts ;;
+    5) MYSQL_BUILD_CXX=clang++-3.9; MYSQL_TEST_SUITES=binlog,binlog_encryption,encryption ;;
+    6) MYSQL_BUILD_CXX=clang++-4.0; MYSQL_TEST_SUITES=csv,federated,funcs_1,funcs_2,gcol,handler,heap,json,maria,percona,perfschema,plugins,multi_source,roles ;;
+# missing default suites: archive,rpl,wsrep
   esac
+  export MYSQL_TEST_SUITES
   export MYSQL_BUILD_CXX MYSQL_BUILD_CC=${MYSQL_BUILD_CXX/++/} MYSQL_COMPILER_LAUNCHER=ccache
 elif [[ "${TRAVIS_OS_NAME}" == 'linux' && "${CXX}" == 'g++' ]]; then
   export MYSQL_BUILD_CXX=g++-${GCC_VERSION};
