ded3b09208e21b374d647059ae23a43466c61fd1
Daniel Black <grooverdan@users.sourceforge.net>
2017-04-03 21:05:52 +1000

    travis: osx multiple images.

--- .travis.compiler.sh
+++ .travis.compiler.sh
@@ -1,17 +1,36 @@
 #!/bin/sh
+set -x -v
 if [[ "${TRAVIS_OS_NAME}" == 'linux' && "${CXX}" == 'clang++' ]]; then
+  CMAKE_OPT="-DWITHOUT_TOKUDB_STORAGE_ENGINE=ON -DWITHOUT_MROONGA_STORAGE_ENGINE=ON";
   case ${GCC_VERSION} in
-    4.8) MYSQL_BUILD_CXX=clang++-3.8; MYSQL_TEST_SUITES=main,optimizer_unfixed_bugs,parts,sys_vars,unit,vcol,innodb,innodb_gis,innodb_zip,innodb_fts ;;
-    5) MYSQL_BUILD_CXX=clang++-3.9; MYSQL_TEST_SUITES=binlog,binlog_encryption,encryption ;;
-    6) MYSQL_BUILD_CXX=clang++-4.0; MYSQL_TEST_SUITES=csv,federated,funcs_1,funcs_2,gcol,handler,heap,json,maria,percona,perfschema,plugins,multi_source,roles ;;
+    4.8) CXX=clang++-3.8; MYSQL_TEST_SUITES=main,optimizer_unfixed_bugs,parts,sys_vars,unit,vcol,innodb,innodb_gis,innodb_zip,innodb_fts ;;
+    5) CXX=clang++-3.9; MYSQL_TEST_SUITES=binlog,binlog_encryption,encryption ;;
+    6) CXX=clang++-4.0; MYSQL_TEST_SUITES=csv,federated,funcs_1,funcs_2,gcol,handler,heap,json,maria,percona,perfschema,plugins,multi_source,roles ;;
 # missing default suites: archive,rpl,wsrep
   esac
-  export MYSQL_TEST_SUITES
-  export MYSQL_BUILD_CXX MYSQL_BUILD_CC=${MYSQL_BUILD_CXX/++/} MYSQL_COMPILER_LAUNCHER=ccache
+  export CXX CC=${CXX/++/}
 elif [[ "${TRAVIS_OS_NAME}" == 'linux' && "${CXX}" == 'g++' ]]; then
-  export MYSQL_BUILD_CXX=g++-${GCC_VERSION};
-  export MYSQL_BUILD_CC=gcc-${GCC_VERSION}
+  CMAKE_OPT=""
+  export CXX=g++-${GCC_VERSION}
+  export CC=gcc-${GCC_VERSION}
+  case ${GCC_VERSION} in
+    4.8) MYSQL_TEST_SUITES=rpl ;;
+    5)   MYSQL_TEST_SUITES=main,archive ; CMAKE_OPT="${CMAKE_OPT} -DCMAKE_BUILD_TYPE=Debug" ;;
+    6)   wget http://mirrors.kernel.org/ubuntu/pool/universe/p/percona-xtradb-cluster-galera-2.x/percona-xtradb-cluster-galera-2.x_165-0ubuntu1_amd64.deb ;
+         ar vx percona-xtradb-cluster-galera-2.x_165-0ubuntu1_amd64.deb ;
+         tar -xvf data.tar.gz ;
+         export WSREP_PROVIDER=$PWD/usr/lib/libgalera_smm.so ;
+         MYSQL_TEST_SUITES=wsrep ;;
+  esac
+else
+  CMAKE_OPT="-DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DCMAKE_C_COMPILER_LAUNCHER=/usr/local/bin/ccache -DCMAKE_CXX_COMPILER_LAUNCHER=/usr/local/bin/ccache -DCMAKE_BUILD_TYPE=Debug"
+  # osx_image based tests
+  env
+  MYSQL_TEST_SUITES=main,innodb
 fi
+
 # main.mysqlhotcopy_myisam consitently failed in travis containers
 # https://travis-ci.org/grooverdan/mariadb-server/builds/217661580
-echo 'main.mysqlhotcopy_myisam : unstable in containers' | tee -a debian/unstable-tests.amd64
+echo 'main.mysqlhotcopy_myisam : unstable in containers' | ${TRAVIS_BUILD_DIR}/mysql-test/unstable-tests
+
+set +x +v
