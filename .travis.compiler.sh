bf861fcbf51b25361c30fee9113e7943da846f24
Daniel Black <grooverdan@users.sourceforge.net>
2017-04-05 06:48:58 +1000

    DebugWithRelInfo, debbuild as matrix include.

--- .travis.compiler.sh
+++ .travis.compiler.sh
@@ -1,44 +1,35 @@
 #!/bin/sh
 set -x -v
-if [[ "${TRAVIS_OS_NAME}" == 'linux' && "${CXX}" == 'clang++' ]]; then
-  CMAKE_OPT="-DWITHOUT_TOKUDB_STORAGE_ENGINE=ON -DWITHOUT_MROONGA_STORAGE_ENGINE=ON";
-  CMAKE_OPT="${CMAKE_OPT} -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
-  case ${GCC_VERSION} in
-    4.8) CXX=clang++-3.8; MYSQL_TEST_SUITES=main,optimizer_unfixed_bugs,parts,sys_vars,unit,vcol,innodb,innodb_gis,innodb_zip,innodb_fts ;;
-    5) CXX=clang++-3.9; MYSQL_TEST_SUITES=binlog,binlog_encryption,encryption ;;
-    6) CXX=clang++-4.0;
+if [[ "${TRAVIS_OS_NAME}" == 'linux' ]]; then
+  if [[ "${CXX}" == 'clang++' ]]; then
+    CMAKE_OPT="-DWITHOUT_TOKUDB_STORAGE_ENGINE=ON -DWITHOUT_MROONGA_STORAGE_ENGINE=ON";
+    CMAKE_OPT="${CMAKE_OPT} -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
+    case ${GCC_VERSION} in
+      4.8) CXX=clang++-3.8 ;;
+      5) CXX=clang++-3.9 ;;
+      6) CXX=clang++-4.0 ;;
+    esac
+    export CXX CC=${CXX/++/}
+  elif [[ "${CXX}" == 'g++' ]]; then
+    CMAKE_OPT=""
+    export CXX=g++-${GCC_VERSION}
+    export CC=gcc-${GCC_VERSION}
+  fi
+  if [[ ${GCC_VERSION} == 6 ]]; then
          wget http://mirrors.kernel.org/ubuntu/pool/universe/p/percona-xtradb-cluster-galera-2.x/percona-xtradb-cluster-galera-2.x_165-0ubuntu1_amd64.deb ;
          ar vx percona-xtradb-cluster-galera-2.x_165-0ubuntu1_amd64.deb ;
          tar -xJvf data.tar.xz ;
          export WSREP_PROVIDER=$PWD/usr/lib/libgalera_smm.so ;
-         MYSQL_TEST_SUITES=wsrep,csv,federated,funcs_1,funcs_2,gcol,handler,heap,json,maria,percona,perfschema,plugins,multi_source,roles ;;
-# missing default suites: archive,rpl,wsrep
-  esac
-  export CXX CC=${CXX/++/}
-elif [[ "${TRAVIS_OS_NAME}" == 'linux' && "${CXX}" == 'g++' ]]; then
-  CMAKE_OPT=""
-  export CXX=g++-${GCC_VERSION}
-  export CC=gcc-${GCC_VERSION}
-  case ${GCC_VERSION} in
-    4.8) MYSQL_TEST_SUITES=rpl ;;
-    5)   MYSQL_TEST_SUITES=main,archive ; CMAKE_OPT="${CMAKE_OPT} -DCMAKE_BUILD_TYPE=Debug" ;;
-    6)   wget http://mirrors.kernel.org/ubuntu/pool/universe/p/percona-xtradb-cluster-galera-2.x/percona-xtradb-cluster-galera-2.x_165-0ubuntu1_amd64.deb ;
-         ar vx percona-xtradb-cluster-galera-2.x_165-0ubuntu1_amd64.deb ;
-         tar -xJvf data.tar.xz ;
-         export WSREP_PROVIDER=$PWD/usr/lib/libgalera_smm.so ;
-         MYSQL_TEST_SUITES=wsrep,csv,federated,funcs_1,funcs_2,gcol,handler,heap,json,maria,percona,perfschema,plugins,multi_source,roles ;;
-  esac
+         MYSQL_TEST_SUITES="${MYSQL_TEST_SUITES},wsrep" ;;
+  fi
 else
   # osx_image based tests
   CMAKE_OPT="-DOPENSSL_ROOT_DIR=/usr/local/opt/openssl"
   CMAKE_OPT="${CMAKE_OPT} -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
   CMAKE_OPT="${CMAKE_OPT} -DWITHOUT_MROONGA_STORAGE_ENGINE=ON"
-  case ${GCC_VERSION} in
-    4.8) MYSQL_TEST_SUITES=rpl ;;
-    5)   MYSQL_TEST_SUITES=main,archive ; CMAKE_OPT="${CMAKE_OPT} -DCMAKE_BUILD_TYPE=Debug -DWITHOUT_TOKUDB_STORAGE_ENGINE=ON" ;;
-    6)   MYSQL_TEST_SUITES=main,innodb ;;
-    *)   MYSQL_TEST_SUITES=csv,federated,funcs_1,funcs_2,gcol,handler,heap,json,maria,percona,perfschema,plugins,multi_source,roles ;;
-  esac
+  if [[ "${TYPE}" == "Debug" ]]; then
+    CMAKE_OPT="${CMAKE_OPT} -DWITHOUT_TOKUDB_STORAGE_ENGINE=ON" ;;
+  fi
 fi
 
 # main.mysqlhotcopy_myisam consitently failed in travis containers
