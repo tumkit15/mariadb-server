9c137fdc3f3770c36290770bfa3ffc80f6c001be
Daniel Black <daniel.black@au.ibm.com>
2017-04-10 13:23:41 +1000

    default log_error for non-systemd systems is @localstatedir@/*.err
    Include instructions for creating a specific logrotate database
    user and specifing it in a global my.cnf file rather than /root/.my.cnf.
    
    This avoids the need to give logrotate access to the root users .my.cnf
    file (ref: https://bugzilla.redhat.com/show_bug.cgi?id=799826).
    
    Signed-off-by: Daniel Black <daniel.black@au.ibm.com>

--- support-files/mysql-log-rotate.sh
+++ support-files/mysql-log-rotate.sh
@@ -1,23 +1,35 @@
-# This logname can be set in /etc/my.cnf
-# by setting the variable "err-log"
-# in the [safe_mysqld] section as follows:
+# This rotates a number of logs of MariaDB
 #
-# [safe_mysqld]
-# err-log=@localstatedir@/mysqld.log
+# Logs need to be enabled in @sysconfdir@/my.cnf
+# or a config file under @sysconf2dir@
 #
-# If the root user has a password you have to create a
-# /root/.my.cnf configuration file with the following
-# content:
+# See the settings:
+# * log_output
+# * log_basename
+# * log_error
+# * slow_query_log
+# * slow_query_log_file
+# * general_log
+# * general_log_file
 #
-# [mysqladmin]
+# Ensure the files specified below match the MariaDB server settings.
+#
+# Also in one of the configuration files add the following
+# group with a user/password as desired.
+# [mysqladmin.logrotate]
+# user= logrotateuser
 # password = <secret> 
-# user= root
 #
 # where "<secret>" is the password. 
 #
-# ATTENTION: This /root/.my.cnf should be readable ONLY
-# for root !
+# Ensure this user has the approprate permissions to rotate the
+# log:
+# CREATE USER logrotateuser@localhost IDENTIFIED BY "<secret>";
+# GRANT RELOAD ON *.* TO logrotateuser@localhost;
+#
+# ATTENTION: This config file should be readable ONLY for root !
 
+@localstatedir@/*.err
 @localstatedir@/mysqld.log
 @localstatedir@/*general.log
 @localstatedir@/*-slow.log {
@@ -30,9 +42,9 @@
     postrotate
 	# just if mysqld is really running
 	if test -x @bindir@/mysqladmin && \
-	   @bindir@/mysqladmin ping &>/dev/null
+	   @bindir@/mysqladmin --defaults-group-suffix=logrotate ping &>/dev/null
 	then
-	   @bindir@/mysqladmin --local flush-error-log \
+	   @bindir@/mysqladmin --defaults-group-suffix=logrotate --local flush-error-log \
               flush-engine-log flush-general-log flush-slow-log
 	fi
     endscript
