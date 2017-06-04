fadb19e7bb1d6494fd3ef0b925939995b5326a8f
Daniel Black <daniel.black@au.ibm.com>
2017-06-05 08:46:40 +1000

    WIP: cmake changes

--- config.h.cmake
+++ config.h.cmake
@@ -270,6 +270,9 @@
 /* this means WITH_VALGRIND - we change some code paths for valgrind */
 #cmakedefine HAVE_valgrind 1
 
+/* detected cache size */
+#cmakedefine CPU_LEVEL1_DCACHE_LINESIZE 1
+
 /* Types we may use */
 #ifdef __APPLE__
   /*
